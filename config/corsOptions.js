a03d2dd016a3444836755263f63eca01f5bd37ef
Mrityunjay <mrityunjaymaharana8@gmail.com>
2024-07-24 21:23:51 +0530

    backened commit

---
+++ config/corsOptions.js
@@ -0,0 +1,28 @@
+// const allowedOrigins = require('./allowedOrigins');
+
+
+// const corsOptions = {
+//     origin: (origin, callback) => {
+
+//         if(allowedOrigins.indexOf(origin) !== -1 || !origin ){
+//             callback(null, true);
+//         }else{
+//             callback(new Error('Not allowed by CORS'));
+//         }
+
+//     },
+//     credentials:true, //cookies, http authentication with cross origin requests
+//     optionsSuccessStatus:200 //(CORS preflight checks)
+// };
+
+// module.exports = corsOptions;
+
+const allowedOrigins = require('./allowedOrigins');
+
+const corsOptions = {
+    origin: true, // Allow all origins
+    credentials: true, // Allow cookies, http authentication with cross-origin requests
+    optionsSuccessStatus: 200 // CORS preflight checks
+};
+
+module.exports = corsOptions;
